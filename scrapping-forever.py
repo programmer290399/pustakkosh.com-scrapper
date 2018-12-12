0e7ded4c9756d33d34ec43c99e6b92b264097bec
Saahil Ali <programmer290399@gmail.com>
2018-12-12 10:12:48 +0530

    Completed  the book scrapping task

--- scrapping-forever.py
+++ scrapping-forever.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from progress.bar import IncrementalBar
 import json 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -27,7 +26,7 @@ product_links = list()
 
 next_button_Xpath = '//*[@id="my-store-741189"]/div[2]/div/div/div[2]/div/div[2]/div/div[2]/div/div[3]/div[2]/a[2]/span[1]'
 
-
+# Here we scrap the links of every single book 
 for link in category_links :
     print('Working on :' ,link.split('/')[-1][:-4])
     try:
@@ -47,7 +46,7 @@ for link in category_links :
     
     
     while True :
-        time.sleep(10)
+        time.sleep(20)
         try :
             
             element = browser.find_element_by_xpath(next_button_Xpath)
@@ -92,11 +91,59 @@ for link in category_links :
 
         except exceptions.StaleElementReferenceException:  
             pass
-    print(len(list(set(product_links))),'Links recived from',link.split('/')[-1][:-4])
+    
     print('writing links to file .... ')
+    out_file.seek(0)
+    out_file.truncate()
     json.dump({'links':list(set(product_links))},out_file)
     print('File successfully updated.....')
 
 
 out_file.close()
 print('All data saved successfully !!')
+
+# Here we scrap book details
+
+links = json.loads(open('book_links.json', 'r' , encoding='utf-8').read())["links"]
+out_file = open('book_Data.json','w+' , encoding='utf-8')
+book_data = dict()
+
+for link in links :
+    
+    try:
+                browser.get(link)
+                time.sleep(15)
+                html_text = browser.page_source
+
+    except Exception as err:
+                print(str(err))
+                break
+    else:
+                print('Successfully Accessed:',link)
+    time.sleep(15)
+    soup = None
+    if html_text is not None:
+            soup = BeautifulSoup(html_text, 'lxml')
+
+    book_name = soup.find('h1', attrs={'class':'product-details__product-title'})
+    if book_name : book_data[book_name.text] = dict()
+    book_price = soup.find('span',attrs={'class':'details-product-price__value notranslate'})
+    if book_price : book_data[book_name.text]['Price'] = book_price.text
+    book_description = soup.find('div',attrs={'id':'productDescription'})
+    if book_description :
+        description_cleaned   = book_description.findChild('div').text.replace('Available for Rent or Used, Second Hand at Best Prices on  www.pustakkosh.com','')
+        book_data[book_name.text]['Decription'] = description_cleaned  
+    img_div = soup.find('div',attrs={'class':'details-gallery__image-wrapper-inner'})
+    if img_div : book_data[book_name.text]['Image_URL'] = img_div.findChild('img')['src']
+    print('writing data to file .... ')
+    out_file.seek(0)
+    out_file.truncate()
+    json.dump(book_data,out_file)
+    print('File successfully updated.....')    
+
+out_file.seek(0)
+out_file.truncate()
+json.dump(book_data,out_file)
+out_file.close()
+print('All Data saved successfully ....!!')
+browser.close()
